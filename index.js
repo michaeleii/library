ec9272245f6a4f8a88a81848508dfa0cfb6fe660
Michael Lei <michaelleie0ca@gmail.com>
2022-12-22 13:05:58 -0800

    Create add to library function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, readStatus) {
 	this.title = title;
 	this.author = author;
@@ -5,3 +7,14 @@ function Book(title, author, pages, readStatus) {
 	this.readStatus = readStatus = readStatus == true ? "read" : "not read yet";
 	this.info = () => console.log(title, author, pages + " pages", readStatus);
 }
+
+const addBookToLibrary = () => {
+	const title = prompt("What is the title of the book?");
+	const author = prompt("Who is the author?");
+	const pages = prompt("How many pages");
+	const readStatus = prompt("Have you finished reading the book?");
+	const book = new Book(title, author, pages, readStatus);
+	myLibrary.push(book);
+};
+
+addBookToLibrary();
